2dd63c54567625f1539e261fb88137f9b9a87f52
Raphael <raphael.antunes@sou.inteli.edu.br>
2023-12-30 12:06:53 -0300

    Aula 15 - Pegando input no terminal

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,13 @@
 const val = require('validator')
 const chalk = require('chalk')
 
-console.log(chalk.green.inverse.bold("Success!"))
+
+const command = process.argv[2]
+
+if (command === "add"){
+    console.log(chalk.green.bold("Success!"))
+    console.log("I'm adding a note")
+} else if (command ===  "remove") {
+    console.log(chalk.red.bold("Error!"))
+    console.log("I'm removing a note")
+}
