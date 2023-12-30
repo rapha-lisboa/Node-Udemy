040df20558fbeebd5eaf6000f073c0f88a66bfa9
Raphael <raphael.antunes@sou.inteli.edu.br>
2023-12-30 12:27:33 -0300

    Aula 16 - Instalando 'yargs'

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,39 @@
 const val = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log("Adding a new note")
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove an existing note',
+    handler: function () {
+        console.log("Removing a note")
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log("Reading the note")
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List the elements of a note',
+    handler: function () {
+        console.log("Listing the elements of the note")
+    }
+})
 
 
-const command = process.argv[2]
-
-if (command === "add"){
-    console.log(chalk.green.bold("Success!"))
-    console.log("I'm adding a note")
-} else if (command ===  "remove") {
-    console.log(chalk.red.bold("Error!"))
-    console.log("I'm removing a note")
-}
+console.log(process.argv)
+console.log(yargs.argv)
