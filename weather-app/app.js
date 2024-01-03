8673bf8079ae5c52d3d660eb9a5ebd6fd4aeef37
Raphael <raphael.antunes@sou.inteli.edu.br>
2024-01-03 02:48:09 -0300

    Aula 32 - Manipulando dados do JSON requisitado por HTTP

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,9 @@
 const request = require('request')
 
-const url = 'https://api.weatherstack.com/current?access_key=156580ac34665b24b5f445f547f2f6d1&query=37.8267,-122.4233'
+const url = 'https://api.weatherstack.com/current?access_key=156580ac34665b24b5f445f547f2f6d1&query=-22.9068,-43.1729&units=m'
 
-request({ url: url }, (error, response) => {
-    const dataJSON = JSON.parse(response.body)
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body.current
 
-    console.log(dataJSON.current)
+    console.log(data.weather_descriptions[0] + ". It is currently", data.temperature, "degrees, but it feels like", data.feelslike, "degrees")
 })
